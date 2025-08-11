77c7d60e63c283c7f89f1d5fe854e17e9724d249
majiCodePhantom <majimeka1212@gmail.com>
2025-08-12 08:57:37 +0900

    問5.txtまで

--- constants.py
+++ constants.py
@@ -50,7 +50,9 @@ RAG_TOP_FOLDER_PATH = "./data"
 SUPPORTED_EXTENSIONS = {
     ".pdf": PyMuPDFLoader,
     ".docx": Docx2txtLoader,
-    ".csv": lambda path: CSVLoader(path, encoding="utf-8")
+    ".csv": lambda path: CSVLoader(path, encoding="utf-8"),
+    ############################.txt追加############################3
+    ".txt":TextLoader
 }
 WEB_URL_LOAD_TARGETS = [
     "https://generative-ai.web-camp.io/"
